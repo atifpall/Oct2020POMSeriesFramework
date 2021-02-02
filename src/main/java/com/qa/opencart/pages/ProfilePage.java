7aa79dbee591a25abc0067d1b8c2fb1063b58c9b
Pall <atifpall@c4b301c97f8b.ant.amazon.com>
2021-02-02 11:44:00 +0530

    created a new profile page class and modfied scripts

---
+++ src/main/java/com/qa/opencart/pages/ProfilePage.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.pages;
+
+import org.apache.log4j.Logger;
+
+import com.qa.opencart.factory.DriverFactory;
+
+public class ProfilePage {
+	
+	private static final Logger LOGGER = Logger.getLogger(String.valueOf(ProfilePage.class));
+	
+	public ProfilePage() {
+		LOGGER.info("inside profile page const...");
+	}
+	
+	
+}
